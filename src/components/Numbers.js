cecec07ae41e77e83a2057775bf6a9090502da5c
ShubhamKumar3010 <shubham8969305655@gmail.com>
2025-01-19 02:50:22 +0530

    "completed Search and Number.json"

---
+++ src/components/Numbers.js
@@ -0,0 +1,94 @@
+import React from 'react'
+
+export default function Numbers() {
+    const containerDivStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '334px',
+    }
+    const textStyle = {
+        fontSize: "14px",
+        fontWeight: 400,
+        lineHeight: "21px",
+        letterSpacing: "1.5px",
+        textAlign: "center",
+        textUnderlinePosition: "from-font",
+        textDecorationSkipInk: "none",
+        color: "#EB8D15"
+    }
+    const textStyle2 = {
+        // fontFamily: "Inter",
+        fontSize: "32px",
+        fontWeight: "700",
+        lineHeight: "38.4px",
+        letterSpacing: "0.1599999964237213px",
+        textAlign: "center",
+        textUnderlinePosition: "from-font",
+        textDecorationSkipInk: "none",
+        marginTop: "5px",
+    }
+    const numberStyle = {
+        fontSize: "42px",
+        fontWeight: 800,
+        lineHeight: "63px",
+        textAlign: "center",
+        textUnderlinePosition: "from-font",
+        textDecorationSkipInk: "none",
+        background: "linear-gradient(96.22deg, #FFA229 9%, #1C4670 59.37%)",
+        WebkitBackgroundClip: "text",
+        WebkitTextFillColor: "transparent"
+    }
+    const numberStylePlus = {
+        fontSize: "25.2px",
+        fontWeight: 800,
+        textAlign: "center",
+        textUnderlinePosition: "from-font",
+        textDecorationSkipInk: "none",
+        background: "linear-gradient(96.22deg, #FFA229 9%, #1C4670 59.37%)",
+        WebkitBackgroundClip: "text",
+        WebkitTextFillColor: "transparent"
+    }
+    const numberHeadingStyle = {
+        fontSize: "16px",
+        fontWeight: "700",
+        lineHeight: "21px",
+        textAlign: "center",
+        textUnderlinePosition: "from-font",
+        textDecorationSkipInk: "none",
+    }
+    return (
+        <div style={containerDivStyle}>
+            <div style={{ display: 'flex', flexDirection: 'column', marginTop: "49px" }}>
+                <text style={textStyle}>WHY REGISTER KARO</text>
+                <text style={textStyle2}>Some Numbers are important</text>
+                <div style={{ display: 'flex', flexDirection: 'row', gap: '150px', marginTop: '54.39px' }}>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                        <div><text style={numberStyle}>1M</text><text style={numberStylePlus}>+</text></div>
+                        <text style={numberHeadingStyle}>CUSTOMERS</text>
+                    </div>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                        <div><text style={numberStyle}>12M</text><text style={numberStylePlus}>+</text></div>
+                        <text style={numberHeadingStyle}>CUSTOMERS</text>
+                    </div>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                        <div><text style={numberStyle}>41M</text><text style={numberStylePlus}>+</text></div>
+                        <text style={numberHeadingStyle}>CUSTOMERS</text>
+                    </div>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                        <div><text style={numberStyle}>78M</text><text style={numberStylePlus}>+</text></div>
+                        <text style={numberHeadingStyle}>CUSTOMERS</text>
+                    </div>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                        <div><text style={numberStyle}>3287M</text><text style={numberStylePlus}>+</text></div>
+                        <text style={numberHeadingStyle}>CUSTOMERS</text>
+                    </div>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                        <div><text style={numberStyle}>4M</text><text style={numberStylePlus}>+</text></div>
+                        <text style={numberHeadingStyle}>CUSTOMERS</text>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Search.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import './Search.css'
+
+export default function Search() {
+    const containerDiv = {
+        height: "330px",
+        background: "linear-gradient(96.22deg, #FFA229 9%, #1C4670 59.37%)"
+    }
+    const searchDiv = {
+        paddingTop: "64px",
+        paddingLeft: "112px",
+        paddingRight: "112px",
+        gap: "32px",
+        opacity: "0px",
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+        color: "#FFFFFF"
+    }
+    const textStyle = {
+        fontFamily: "Inter",
+        lineHeight: "20px",
+        letterSpacing: "-0.10000000149011612px",
+        textUnderlinePosition: "from-font",
+        textDecorationSkipInk: "none",
+    }
+    const text2Style = {
+        fontFamily: "Inter",
+        lineHeight: "40px",
+        letterSpacing: "-0.015em",
+        textAlign: "center",
+        textUnderlinePosition: "from-font",
+        textDecorationSkipInk: "none",
+    }
+    return (
+        <div style={containerDiv}>
+            <div style={searchDiv}>
+                <text style={{
+                    textStyle, fontSize: "14px",
+                    fontWeight: "600",
+                }}>
+                    1% OF THE INDUSTRY
+                </text>
+                <text style={{ text2Style, fontSize: "32px", fontWeight: "700", }}>
+                    Welcome to your new digital reality. Now.
+                </text>
+                <div style={{ display: "flex", flexDirection: "row", maxWidth: "512px", width: "100%", background: "#FFFFFF", borderRadius: "6px", }}>
+                    <input style={{ maxWidth: "420px", width: "100%", borderRadius: "6px 0px 0px 6px", padding: "12px 16px 12px 16px" }} placeholder='Enter you email' >
+                    </input>
+                    <button style={{ height: "46px", maxWidth: "120px", width: "100%", borderRadius: "0px 6px 6px 0px", background: "#FFA229", color: "#FFFFFF", }}>Submit</button>
+                </div>
+                <div style={{
+                    width: "Hug(641px)px",
+                    height: "Hug(24px)px",
+                    gap: "24px",
+                    display: "flex",
+                    flexDirection: "row",
+                }}>
+                    <div style={{ display: "flex", flexDirection: "row", gap: "16px", alignItems: "center", justifyContent: "center" }}>
+                        <input type="checkbox" className="circle-checkbox" />
+                        <text>Instant results</text>
+                    </div>
+                    <div style={{ display: "flex", flexDirection: "row", gap: "16px", alignItems: "center", justifyContent: "center" }}>
+                        <input type="checkbox" className="circle-checkbox" />
+                        <text>User-friendly interface</text>
+                    </div>
+                    <div style={{ display: "flex", flexDirection: "row", gap: "16px", alignItems: "center", justifyContent: "center" }}>
+                        <input type="checkbox" className="circle-checkbox" />
+                        <text>Personalized customization</text>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
